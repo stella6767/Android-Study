fd1a26fea8060c7e2972392a060fcf5c90f2870e
newbie-newb <userA@my_email.com>
2021-02-15 17:32:05 +0900

    constarinLayout 실습

---
+++ InaragramLoginPage(Constrain)/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //그림 그리는 함수
+        Log.d(TAG, "onCreate: ");
+    }
+}
